57fd762bc782fb7879ffb3ef0eb3a45d6a217299
denisa-valentina <denisa-valentina.aeloaiei@student.tuiasi.ro>
2024-11-28 14:38:11 +0200

    update interfata

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,4 @@
 import multiprocessing
-import threading
 from multiprocessing import Process, Pipe
 from Code.Client import  Client
 from Client_Connect_Interface import *
@@ -29,8 +28,8 @@ def main():
     client_proc = Process(target=client.operation)
     client_proc.start()
 
-    client_interface_thread = threading.Thread(target=run_client_interface, args=(queue,))
-    client_interface_thread.start()
+    client_interface_proc = Process(target=run_client_interface, args=(queue,))
+    client_interface_proc.start()
 
     try:
         while True:
@@ -57,7 +56,7 @@ def main():
     except BaseException:
         print("The application suddenly stopped.")
     finally:
-        client_interface_thread.join()
+        client_interface_proc.join()
         client_proc.join()
         print("The app is closing.")
 
